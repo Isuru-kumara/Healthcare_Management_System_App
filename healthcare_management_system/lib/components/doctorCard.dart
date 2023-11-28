958d02c4234dc1f5dcbbd88ecc0a4fab7b487a50
Isuru Kumara (Hirun Kolongahapitiya) <isurukumara07194@gmail.com>
2023-11-27 21:49:18 -0800

    Created a Frontend

---
+++ healthcare_management_system/lib/components/doctorCard.dart
@@ -0,0 +1,102 @@
+import 'package:healthcare_management_system/utils/config.dart';
+import 'package:flutter/material.dart';
+
+class DoctorCard extends StatelessWidget {
+  const DoctorCard({
+    Key? key,
+    required this.route,
+    required this.doctor,
+    //required this.isFav,
+  }) : super(key: key);
+
+  final String route;
+  final Map<String, dynamic> doctor;
+  //final bool isFav;
+
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      height: 150,
+      child: GestureDetector(
+        child: Card(
+          elevation: 5,
+          color: Colors.white,
+          child: Row(
+            children: [
+              SizedBox(
+                width: Config.widthSize * 0.28,
+                height: Config.heightSize * 0.28,  
+                child: Image.network(
+                  //"Assets/doctor_3.jpg",
+                  "http://192.168.43.214:8000${doctor['doctor_profile']}",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        //"Dr. Nishantha Kumara",
+                        "Dr. ${doctor['doctor_name']}",
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        //"Fever",
+                        "${doctor['category']}",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const <Widget>[
+                          Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 16,
+                          ),
+                          Spacer(
+                            flex: 1,
+                          ),
+                          Text('4.5'),
+                          Spacer(
+                            flex: 1,
+                          ),
+                          Text('Reviews'),
+                          Spacer(
+                            flex: 1,
+                          ),
+                          Text('(20)'),
+                          Spacer(
+                            flex: 7,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        onTap: () {          
+          Navigator.of(context).pushNamed(
+            route, arguments: doctor
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ healthcare_management_system/lib/screens/doctorDetails.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+import 'package:healthcare_management_system/components/button.dart';
+import '../components/customAppBar.dart';
+import '../utils/config.dart';
+
+class DoctorDetails extends StatefulWidget {
+  const DoctorDetails({Key? key, /*required this.doctor, required this.isFav*/})
+        : super(key: key);
+
+  /*final Map<String, dynamic> doctor;
+  final isFav;*/
+
+  @override
+  State<DoctorDetails> createState() => DoctorDetailsState();
+}
+
+class DoctorDetailsState extends State<DoctorDetails> {
+  //Map<String, dynamic> doctor = {};
+  bool isFav = false;
+
+  /*@override
+  void initState() {
+    doctor = widget.doctor;
+    isFav = widget.isFav;
+    super.initState();
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    final doctor = ModalRoute.of(context)!.settings.arguments as Map;
+    Config().init(context);
+    return Scaffold(
+      appBar: CustomAppBar(
+        appTitle: "Doctor Details",
+        icon: const Icon(Icons.arrow_back_ios),
+        //icon: const Icon(FontAwesomeIcons.arrowUpFromBracket),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              setState(() {
+                isFav = !isFav;
+              });
+            },
+            icon: Icon(
+              //icon: FaIcon(
+              //isFav ? Icons.favourite_rounded : Icons.favourite_outline,
+              isFav ? Icons.favorite : Icons.favorite_border_outlined,
+              color: Colors.blue,
+            ),
+          )     
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            DetailHead(
+              doctor:doctor,
+            ),
+            DetailBody(
+              doctor:doctor,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Button(
+                width: double.infinity,
+                title: "Book Appointment",
+                onPressed: () {
+                  Navigator.of(context).pushNamed("schedule",
+                      arguments: {"doctor_id": doctor["doctor_id"]}
+                  );
+                },
+                disable: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailHead extends StatelessWidget {
+  const DetailHead({Key? key, required this.doctor}) : super(key: key);
+
+  final Map<dynamic, dynamic> doctor;
+
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+          children: <Widget>[
+            Config.spaceMedium,
+            CircleAvatar(
+              radius: 65.0,
+             //backgroundImage: AssetImage("Assets/doctor_9.jpg"),
+              backgroundImage: NetworkImage("http://192.168.43.214:8000${doctor['doctor_profile']}",),
+              backgroundColor: Colors.white,
+            ),
+            Config.spaceMedium,
+            Text(
+              //"Dr. Wasana Kumari",
+              "Dr. ${doctor['doctor_name']}",
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Config.spaceSmall,
+            SizedBox(
+              width: Config.widthSize * 0.75,
+              child: Text(
+                //"MBBS (International Medical University, Russia), MRCP (Royal College of Physicians, United Kingdom)",
+                "${doctor['bio_data']}",
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Config.spaceSmall,
+            SizedBox(
+              width: Config.widthSize * 0.75,
+              child: const Text(
+                "Colombo General Hospital",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Config.spaceSmall,
+          ],
+      ),
+    );
+  }
+}
+
+
+class DetailBody extends StatelessWidget {
+  const DetailBody({Key? key, required this.doctor}) : super(key: key);
+
+  final Map<dynamic, dynamic> doctor;
+
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.only(bottom: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          DoctorInfo(
+            patients: doctor["patients"],
+            exp: doctor["experience"],
+          ),
+          Config.spaceSmall,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const <Widget>[
+              Text(
+                "Channeling Fee",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                ),
+                softWrap: true,
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                "Rs. 350",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
+                ),
+                softWrap: true,
+                textAlign: TextAlign.right,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DoctorInfo extends StatelessWidget {
+  const DoctorInfo({Key? key, required this.patients, required this.exp})
+      : super(key: key);
+
+  final int patients;
+  final int exp;
+
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    return Row(
+      children: <Widget>[
+        InfoCard(
+            label: "Patients",
+            //value: "98",
+            value: "$patients",
+        ),
+        const SizedBox(width: 15,),
+        InfoCard(
+          label: "Experience",
+          //value: "6 Years",
+          value: "$exp Years",
+        ),
+        const SizedBox(width: 15,),
+        const InfoCard(
+          label: "Ratings",
+          value: "4.5",
+        ),
+      ],
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({Key? key, required this.label, required this.value}) 
+      : super(key: key);
+
+  final String label;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Config.primaryColor,
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 30,
+          horizontal: 15
+        ),
+        child: Column(
+          children: <Widget>[
+            Text(
+              label,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              value,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
